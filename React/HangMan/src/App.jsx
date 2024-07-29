58970e17891ca222f0a91c0e9b966c514502e665
Natwar Patidar <natvarp6062@gmail.com>
2024-07-29 15:56:00 +0530

    add codes

--- React/HangMan/src/App.jsx
+++ React/HangMan/src/App.jsx
@@ -7,6 +7,7 @@ function App () {
       <Button text="Primary" buttonType="submit" styleType="primary" onClickHandler={() => console.log('Primary Clicked')} />
       <Button text="Secondary" buttonType="buttton" styleType="secondary" onClickHandler={() => console.log('Secondary Clicked')} />
       <Button text="Warning" buttonType="buttton" styleType="warning" onClickHandler={() => console.log('Warning Clicked')} />
+      <Button text="Error" buttonType="buttton" styleType="error" onClickHandler={() => console.log('Error Clicked')} />
     </div>
   );
 }
--- React/HangMan/src/components/Button/getButtonStyleType.js
+++ React/HangMan/src/components/Button/getButtonStyleType.js
@@ -2,6 +2,7 @@ function getButtonStyling (styleType) {
     const primaryButtonStyling = 'bg-blue-500 border-blue-700 text-white hover:bg-blue-700 hover:border-blue-900';
     const secondaryButtonStyling = 'bg-green-500 border-green-700 text-white hover:bg-green-600 hover:border-green-900';
     const warningButtonStyling = 'bg-yellow-400 border-blue-700 text-white hover:bg-yellow-500 hover:border-yellow-900';
+    const errorButtonStyling = 'bg-red-400 border-red-700 text-white hover:bg-red-500 hover:border-red-900';
 
     if(styleType === "primary") {
         return primaryButtonStyling;
@@ -9,6 +10,8 @@ function getButtonStyling (styleType) {
         return secondaryButtonStyling;
     } else if (styleType === "warning") {
         return warningButtonStyling;
+    } else if (styleType === 'error') {
+        return errorButtonStyling;
     }
 
 }
